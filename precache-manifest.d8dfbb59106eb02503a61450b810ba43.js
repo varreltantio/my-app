6251831e9c21623fcca94beb42701c4c73c56957
VarrelTantio <snetid90@gmail.com>
2020-06-24 10:38:59 +0700

    Updates

---
+++ precache-manifest.d8dfbb59106eb02503a61450b810ba43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02305d51cfbc84c019ac39241cf127d4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e8a8d2d31a56bcea429c",
+    "url": "/my-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "0b23082f3e07d6cb0571",
+    "url": "/my-app/static/js/2.28906092.chunk.js"
+  },
+  {
+    "revision": "e8a8d2d31a56bcea429c",
+    "url": "/my-app/static/js/main.cc0bc543.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
